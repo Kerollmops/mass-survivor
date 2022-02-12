a22eb8b18f91cd6ab05b4c86a329098b0432c2be
Clément Renault <clement@meilisearch.com>
2022-02-12 20:37:09 +0100

    First basic one shot demons animation

--- src/assets.rs
+++ src/assets.rs
@@ -2,16 +2,25 @@ use bevy::prelude::*;
 use bevy_asset_loader::AssetCollection;
 
 #[derive(AssetCollection)]
-pub struct IconsetAssets {
-    #[asset(texture_atlas(tile_size_x = 32., tile_size_y = 32., columns = 18, rows = 50))]
-    #[asset(path = "images/iconset_fantasy_standalone.png")]
-    pub iconset_fantasy_standalone: Handle<TextureAtlas>,
+pub struct EnemiesAssets {
+    #[asset(texture_atlas(
+        tile_size_x = 16.,
+        tile_size_y = 16.,
+        columns = 12,
+        rows = 3,
+        padding_x = 2.,
+        padding_y = 2.,
+    ))]
+    #[asset(path = "images/small_demons.png")]
+    pub small_demons: Handle<TextureAtlas>,
 
-    #[asset(texture_atlas(tile_size_x = 32., tile_size_y = 32., columns = 18, rows = 50))]
-    #[asset(path = "images/iconset_fantasy_castshadows.png")]
-    pub iconset_fantasy_castshadows: Handle<TextureAtlas>,
-
-    #[asset(texture_atlas(tile_size_x = 32., tile_size_y = 32., columns = 10, rows = 4))]
-    #[asset(path = "images/iconset_halloween_standalone.png")]
-    pub iconset_halloween_standalone: Handle<TextureAtlas>,
+    #[asset(texture_atlas(
+        tile_size_x = 32.,
+        tile_size_y = 32.,
+        columns = 4,
+        rows = 1,
+        padding_x = 22.,
+    ))]
+    #[asset(path = "images/big_demons.png")]
+    pub big_demons: Handle<TextureAtlas>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use std::f32::consts::PI;
+use std::time::Duration;
 
-use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use bevy_asset_loader::AssetLoader;
+use bevy_tweening::lens::*;
+use bevy_tweening::*;
 use heron::prelude::*;
+use rand::Rng;
 
 use self::assets::*;
-use self::enemies::*;
-use self::helper::*;
 
 mod assets;
-mod enemies;
-mod game_sprites;
-mod helper;
 
 const MAP_SIZE: u32 = 41;
 const GRID_WIDTH: f32 = 0.05;
@@ -21,38 +18,28 @@ const PLAYER_SPEED: f32 = 10.0;
 const HEALTHY_PLAYER_COLOR: Color = Color::rgb(0., 0.47, 1.);
 const HIT_PLAYER_COLOR: Color = Color::rgb(0.9, 0.027, 0.);
 
-const AXE_HEAD_COLOR: Color = Color::rgb(0.52, 0.62, 0.8);
-const AXE_HEAD_SPEED: f32 = 2.; // radian/s
-
 fn main() {
     let mut app = App::new();
     AssetLoader::new(MyStates::AssetLoading)
         .continue_to_state(MyStates::Next)
-        .with_collection::<IconsetAssets>()
+        .with_collection::<EnemiesAssets>()
         .build(&mut app);
 
     app.add_state(MyStates::AssetLoading)
         .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
         .add_plugins(DefaultPlugins)
+        .add_plugin(TweeningPlugin)
         .add_plugin(PhysicsPlugin::default())
         .insert_resource(Gravity::from(Vec3::ZERO))
         .add_startup_system(setup)
         .add_startup_system(spawn_player)
         .add_system_to_stage(CoreStage::PostUpdate, camera_follow)
+        .add_system_set(SystemSet::on_enter(MyStates::Next).with_system(spawn_ennemies))
         .add_system_set(
             SystemSet::on_update(MyStates::Next)
                 .with_system(move_player)
-                .with_system(tracking_movement)
-                .with_system(slow_walking_movement)
-                .with_system(running_group_movement)
-                .with_system(spawn_enemy_waves)
-                .with_system(create_loot)
-                .with_system(player_loot_stuff)
-                .with_system(axe_head_kill_ennemies)
-                .with_system(rotate_axe_head)
                 .with_system(change_player_color)
-                .with_system(player_loot_gems)
-                .with_system(gems_player_attraction),
+                .with_system(player_loot_gems),
         )
         .run();
 }
@@ -61,48 +48,6 @@ fn setup(mut commands: Commands) {
     let mut camera_bundle = OrthographicCameraBundle::new_2d();
     camera_bundle.orthographic_projection.scale = 1. / 50.;
     commands.spawn_bundle(camera_bundle);
-    commands.insert_resource(LootAllGemsFor(Timer::from_seconds(0., false)));
-
-    // Setup enemy waves
-    commands.spawn_bundle(EnemyWaveBundle {
-        kind: EnemyKind::BlueFish,
-        timer: Timer::from_seconds(3., false),
-        size: EnemyWaveSize(40),
-        count: EnemyWavesCount(2),
-        movement_kind: MovementKind::Tracking,
-    });
-
-    commands.spawn_bundle(EnemyWaveBundle {
-        kind: EnemyKind::Pumpkin,
-        timer: Timer::from_seconds(10., true),
-        size: EnemyWaveSize(10),
-        count: EnemyWavesCount(3),
-        movement_kind: MovementKind::SlowWalking,
-    });
-
-    commands.spawn_bundle(EnemyWaveBundle {
-        kind: EnemyKind::SkeletonHead,
-        timer: Timer::from_seconds(15., false),
-        size: EnemyWaveSize(30),
-        count: EnemyWavesCount(1),
-        movement_kind: MovementKind::RunningGroup,
-    });
-
-    commands.spawn_bundle(EnemyWaveBundle {
-        kind: EnemyKind::BigRedFish,
-        timer: Timer::from_seconds(25., false),
-        size: EnemyWaveSize(30),
-        count: EnemyWavesCount(2),
-        movement_kind: MovementKind::Tracking,
-    });
-
-    commands.spawn_bundle(EnemyWaveBundle {
-        kind: EnemyKind::Knife,
-        timer: Timer::from_seconds(35., false),
-        size: EnemyWaveSize(40),
-        count: EnemyWavesCount(2),
-        movement_kind: MovementKind::RunningGroup,
-    });
 
     // Horizontal lines
     for i in 0..=MAP_SIZE {
@@ -139,11 +84,87 @@ fn setup(mut commands: Commands) {
     }
 }
 
+fn spawn_ennemies(mut commands: Commands, enemies_assets: Res<EnemiesAssets>) {
+    let small_demons_count = 12 * 3;
+    let mut rng = rand::thread_rng();
+
+    let start = Vec3::new(-5., 5., 90.);
+    let left_to_right = Tween::new(
+        EaseMethod::Linear,
+        TweeningType::Once,
+        Duration::from_secs(4),
+        OneAxisTransformPositionLens { slide_on: Axis::X, start, end: Vec3::new(5., 5., 90.) },
+    );
+
+    let top_to_bottom = Tween::new(
+        EaseMethod::Linear,
+        TweeningType::Once,
+        Duration::from_millis(50),
+        OneAxisTransformPositionLens {
+            slide_on: Axis::Y,
+            start: Vec3::new(5., 5., 90.),
+            end: Vec3::new(5., 5. - 0.8, 90.),
+        },
+    );
+
+    let right_to_left = Tween::new(
+        EaseMethod::Linear,
+        TweeningType::Once,
+        Duration::from_secs(4),
+        OneAxisTransformPositionLens {
+            slide_on: Axis::X,
+            start: Vec3::new(5., 5. - 0.8, 90.),
+            end: start - Vec3::new(0., 0.8, 0.),
+        },
+    );
+
+    let tween = left_to_right.then(top_to_bottom).then(right_to_left);
+
+    commands
+        .spawn_bundle(FleetBundle {
+            transform: Transform::from_translation(start),
+            ..Default::default()
+        })
+        .insert(Animator::new(tween))
+        .with_children(|parent| {
+            for column in -5..=5 {
+                for row in -2..=1 {
+                    let position = Vec3::new(column as f32, row as f32, 0.) * 1.2;
+                    let index = rng.gen_range(0..small_demons_count);
+
+                    parent
+                        .spawn_bundle(SpriteSheetBundle {
+                            transform: Transform::from_translation(position),
+                            sprite: TextureAtlasSprite {
+                                index,
+                                custom_size: Some(Vec2::new(1., 1.)),
+                                ..Default::default()
+                            },
+                            texture_atlas: enemies_assets.small_demons.clone(),
+                            ..Default::default()
+                        })
+                        .insert(Velocity::default())
+                        .insert(RigidBody::KinematicPositionBased)
+                        .insert(CollisionShape::Cuboid {
+                            half_extends: Vec3::splat(0.5),
+                            border_radius: None,
+                        })
+                        .insert(RotationConstraints::lock())
+                        .insert(
+                            CollisionLayers::none()
+                                .with_group(GameLayer::Enemies)
+                                .with_mask(GameLayer::Player),
+                        )
+                        .insert(Enemy);
+                }
+            }
+        });
+}
+
 fn spawn_player(mut commands: Commands) {
-    let player_pos = Vec3::new(0., 0., 100.);
     commands
         .spawn_bundle(SpriteBundle {
-            transform: Transform::from_translation(player_pos),
+            transform: Transform::from_translation(Vec3::new(0., 0., 100.)),
             sprite: Sprite {
                 color: HEALTHY_PLAYER_COLOR,
                 custom_size: Some(Vec2::new(1., 1.)),
@@ -160,28 +181,7 @@ fn spawn_player(mut commands: Commands) {
             GameLayer::Stuff,
             GameLayer::Enemies,
         ]))
-        .insert(Player::default())
-        .with_children(|commands| {
-            commands
-                .spawn_bundle(SpriteBundle {
-                    transform: Transform::from_translation(Vec3::new(0., -3., 0.)),
-                    sprite: Sprite {
-                        color: AXE_HEAD_COLOR,
-                        custom_size: Some(Vec2::new(0.8, 0.8)),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                })
-                .insert(RigidBody::Static)
-                .insert(CollisionShape::Sphere { radius: 0.6 })
-                .insert(
-                    CollisionLayers::none()
-                        .with_group(GameLayer::Weapon)
-                        .with_mask(GameLayer::Enemies),
-                )
-                .insert(RotationRadian(0.))
-                .insert(AxeHead);
-        });
+        .insert(Player::default());
 }
 
 fn move_player(keys: Res<Input<KeyCode>>, mut player_query: Query<&mut Velocity, With<Player>>) {
@@ -206,26 +206,6 @@ fn move_player(keys: Res<Input<KeyCode>>, mut player_query: Query<&mut Velocity,
     }
 }
 
-fn rotate_axe_head(
-    time: Res<Time>,
-    mut axe_head_query: Query<
-        (&mut Transform, &mut RotationRadian),
-        (With<AxeHead>, Without<Player>),
-    >,
-) {
-    let (mut transform, mut rotation) = match axe_head_query.iter_mut().next() {
-        Some(transform) => transform,
-        None => return,
-    };
-
-    let radian = rotation.0 + AXE_HEAD_SPEED * time.delta_seconds();
-    rotation.0 = if radian >= 2. * PI { 0. } else { radian };
-
-    let x = rotation.0.cos() * 3.;
-    let y = rotation.0.sin() * 3.;
-    transform.translation = Vec3::new(x, y, 0.);
-}
-
 fn change_player_color(
     mut events: EventReader<CollisionEvent>,
     mut player_query: Query<(Entity, &mut Sprite), With<Player>>,
@@ -248,82 +228,6 @@ fn change_player_color(
     }
 
     player_sprite.color = HEALTHY_PLAYER_COLOR;
-}
-
-fn create_loot(
-    mut commands: Commands,
-    iconset_assets: Res<IconsetAssets>,
-    mut player_query: Query<(&mut Player, &Transform)>,
-) {
-    let (mut player, transform) = match player_query.iter_mut().next() {
-        Some(value) => value,
-        None => return,
-    };
-
-    let mut rng = rand::thread_rng();
-    if !player.lvl1_stuff_generated && player.xp >= 30 {
-        player.lvl1_stuff_generated = true;
-        let pos = random_in_radius(&mut rng, transform.translation, 5.);
-        let pos = move_from_deadzone(pos, 3.).extend(95.);
-
-        commands
-            .spawn_bundle(SpriteSheetBundle {
-                transform: Transform::from_translation(pos).with_scale(Vec3::splat(0.04)),
-                sprite: TextureAtlasSprite::new(864),
-                texture_atlas: iconset_assets.iconset_fantasy_castshadows.clone(),
-                ..Default::default()
-            })
-            .insert(RigidBody::Sensor)
-            .insert(CollisionShape::Cuboid { half_extends: Vec3::splat(0.5), border_radius: None })
-            .insert(
-                CollisionLayers::none().with_group(GameLayer::Stuff).with_mask(GameLayer::Player),
-            )
-            .insert(Stuff::FishingRod);
-    }
-}
-
-fn axe_head_kill_ennemies(
-    mut commands: Commands,
-    iconset_assets: Res<IconsetAssets>,
-    mut events: EventReader<CollisionEvent>,
-    ennemies_query: Query<&Transform>,
-) {
-    events
-        .iter()
-        .filter(|e| e.is_started())
-        .filter_map(|event| {
-            let (entity_1, entity_2) = event.rigid_body_entities();
-            let (layers_1, layers_2) = event.collision_layers();
-            if is_weapon_layer(layers_1) && is_enemy_layer(layers_2) {
-                Some(entity_2)
-            } else if is_weapon_layer(layers_2) && is_enemy_layer(layers_1) {
-                Some(entity_1)
-            } else {
-                None
-            }
-        })
-        .for_each(|enemy_entity| {
-            if let Ok(transform) = ennemies_query.get_component::<Transform>(enemy_entity) {
-                let pos = transform.translation.xy().extend(80.0);
-                commands.entity(enemy_entity).despawn();
-                commands
-                    .spawn_bundle(SpriteSheetBundle {
-                        transform: Transform::from_translation(pos).with_scale(Vec3::splat(0.015)),
-                        sprite: TextureAtlasSprite::new(474), // blue diamond
-                        texture_atlas: iconset_assets.iconset_fantasy_standalone.clone(),
-                        ..Default::default()
-                    })
-                    .insert(MoveToPlayer::default())
-                    .insert(RigidBody::Sensor)
-                    .insert(CollisionShape::Sphere { radius: 0.25 })
-                    .insert(
-                        CollisionLayers::none()
-                            .with_group(GameLayer::Gem)
-                            .with_mask(GameLayer::Player),
-                    )
-                    .insert(Gem);
-            }
-        });
 }
 
 fn player_loot_gems(
@@ -353,73 +257,12 @@ fn player_loot_gems(
         });
 }
 
-fn player_loot_stuff(
-    mut commands: Commands,
-    mut loot_all_gems: ResMut<LootAllGemsFor>,
-    mut events: EventReader<CollisionEvent>,
-    stuff_query: Query<&Stuff>,
-) {
-    events
-        .iter()
-        .filter(|e| e.is_started())
-        .filter_map(|event| {
-            let (entity_1, entity_2) = event.rigid_body_entities();
-            let (layers_1, layers_2) = event.collision_layers();
-            if is_player_layer(layers_1) && is_stuff_layer(layers_2) {
-                Some(entity_2)
-            } else if is_player_layer(layers_2) && is_stuff_layer(layers_1) {
-                Some(entity_1)
-            } else {
-                None
-            }
-        })
-        .for_each(|entity| {
-            if let Ok(Stuff::FishingRod) = stuff_query.get_component::<Stuff>(entity) {
-                *loot_all_gems = LootAllGemsFor(Timer::from_seconds(5., false));
-            }
-            commands.entity(entity).despawn();
-        });
-}
-
 fn is_player_layer(layers: CollisionLayers) -> bool {
     layers.contains_group(GameLayer::Player)
 }
 
-fn is_weapon_layer(layers: CollisionLayers) -> bool {
-    layers.contains_group(GameLayer::Weapon)
-}
-
-fn is_enemy_layer(layers: CollisionLayers) -> bool {
-    layers.contains_group(GameLayer::Enemies)
-}
-
-fn is_stuff_layer(layers: CollisionLayers) -> bool {
-    layers.contains_group(GameLayer::Stuff)
-}
-
 fn is_gem_layer(layers: CollisionLayers) -> bool {
     layers.contains_group(GameLayer::Gem)
-}
-
-fn gems_player_attraction(
-    time: Res<Time>,
-    mut loot_all_gems: ResMut<LootAllGemsFor>,
-    player_query: Query<&Transform, With<Player>>,
-    mut gems_query: Query<(&mut Transform, &mut MoveToPlayer), (With<Gem>, Without<Player>)>,
-) {
-    let player_transform = player_query.single();
-
-    loot_all_gems.0.tick(time.delta());
-
-    for (mut transform, mut move_to_player) in gems_query.iter_mut() {
-        let dist = player_transform.translation.xy().distance(transform.translation.xy());
-        if move_to_player.0 || !loot_all_gems.0.finished() || dist <= 2.0 {
-            move_to_player.0 = true;
-            let dir = (player_transform.translation.xy() - transform.translation.xy())
-                .normalize_or_zero();
-            transform.translation += (dir * time.delta_seconds() * 15.0).extend(0.);
-        }
-    }
 }
 
 fn camera_follow(
@@ -442,9 +285,21 @@ enum MyStates {
     Next,
 }
 
+#[derive(Component)]
+pub struct Enemy;
+
+#[derive(Default, Component)]
+pub struct Fleet;
+
+#[derive(Default, Bundle)]
+pub struct FleetBundle {
+    transform: Transform,
+    global_transform: GlobalTransform,
+    _fleet: Fleet,
+}
+
 #[derive(Default, Component)]
 pub struct Player {
-    lvl1_stuff_generated: bool,
     xp: usize,
 }
 
@@ -458,9 +313,6 @@ pub enum Stuff {
 
 #[derive(Component)]
 pub struct AxeHead;
-
-#[derive(Component)]
-pub struct LootAllGemsFor(Timer);
 
 #[derive(Component)]
 pub struct RotationRadian(f32);
@@ -478,4 +330,35 @@ pub enum GameLayer {
     Enemies,
     Gem,
     Stuff,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum Axis {
+    X,
+    Y,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct OneAxisTransformPositionLens {
+    slide_on: Axis,
+    start: Vec3,
+    end: Vec3,
+}
+
+impl Lens<Transform> for OneAxisTransformPositionLens {
+    fn lerp(&mut self, target: &mut Transform, ratio: f32) {
+        let value = self.start + (self.end - self.start) * ratio;
+
+        let axis = match self.slide_on {
+            Axis::X => target.translation[1],
+            Axis::Y => target.translation[0],
+        };
+
+        target.translation = value;
+
+        match self.slide_on {
+            Axis::X => target.translation[1] = axis,
+            Axis::Y => target.translation[0] = axis,
+        }
+    }
 }
